31a45911ce920a7c4e28f583f9393647668b33cc
Viktor Lindqvist <lindqvistcs@gmail.com>
2025-12-29 21:42:27 +0100

    Added new images to config & added page for the game (checkout atm)

--- config/products_config.js
+++ config/products_config.js
@@ -4,98 +4,113 @@ const products = [
     {
         title: "Battlefield™ 1 Revolution",
         genre: "Action",
-        image: "./img/products/battlefield-1.jpg",
+        image: "./img/products/battlefield-1.webp",
+        page: "./checkout.html",
         price: 499
     },
     {
         title: "Grand Theft Auto V Enhanced",
         genre: "Action",
-        image: "./img/products/gtav.jpg",
+        image: "./img/products/gtav.webp",
+        page: "./checkout.html",
         price: 299
     },
     {
         title: "Ready or Not",
         genre: "Action",
-        image: "./img/products/readyornot.png",
+        image: "./img/products/readyornot.webp",
+        page: "./checkout.html",
         price: 599
     },
     {
         title: "Borderlands 4",
         genre: "Action",
-        image: "./img/products/borderlands4.jpg",
+        image: "./img/products/borderlands4.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 299
     },
     {
         title: "EA Sports FC 26",
         genre: "Sport",
-        image: "./img/products/fc26.jpg",
+        image: "./img/products/fc26.webp",
+        page: "./checkout.html",
         price: 499
     },
     {
         title: "Arc Raiders",
         genre: "Action",
-        image: "./img/products/arc-raiders.jpg",
+        image: "./img/products/arc-raiders.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 299
     },
     {
         title: "Out of Worlds",
         genre: "RPG",
-        image: "./img/products/outofworlds.jpg",
+        image: "./img/products/outofworlds.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 299
     },
     {
         title: "Hogwarts Legacy",
         genre: "RPG",
-        image: "./img/products/hogwartslegacy.jpg",
+        image: "./img/products/hogwartslegacy.webp",
+        page: "./checkout.html",
         price: 349
     },
     {
         title: "Red Dead Redemption 2",
         genre: "Action",
-        image: "./img/products/reddeadredemption2.jpg",
+        image: "./img/products/reddeadredemption2.webp",
+        page: "./checkout.html",
         price: 199
     },
     {
         title: "Subnautica 2",
         genre: "Adventure",
-        image: "./img/products/subnautica-2.png",
+        image: "./img/products/subnautica-2.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 299
     },
     {
         title: "The Bornless",
         genre: "Horror",
-        image: "./img/products/bornless.png",
+        image: "./img/products/bornless.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 459
     },
     {
         title: "ARK: Survival Ascended",
         genre: "Adventure",
-        image: "./img/products/ark-survival-ascended.jpg",
+        image: "./img/products/ark-survival-ascended.webp",
+        page: "./checkout.html",
         price: 499
     },
     {
         title: "Bloodstained",
         genre: "RPG",
-        image: "./img/products/bloodstained.jpg",
+        image: "./img/products/bloodstained.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 245
     },
     {
         title: "We Were Here Together",
         genre: "CO-OP",
-        image: "./img/products/wewerehere.png",
+        image: "./img/products/wewerehere.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 100
     },
     {
         title: "Hades II",
         genre: "RPG",
-        image: "./img/products/hadesii.png",
+        image: "./img/products/hadesii.webp",
+        page: "./checkout.html",
         price: 499,
         onSalePrice: 299
     }
