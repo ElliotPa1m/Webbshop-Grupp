6ef5b4ca311529f22cb006a3c75d4c90c0d1f16d
Viktor Lindqvist <lindqvistcs@gmail.com>
2025-12-29 16:08:29 +0100

    Added products config

---
+++ config/products_config.js
@@ -0,0 +1,102 @@
+// This is the configuration for all products on the site. Its sole purpose is to make it easier for everyone to change what games that are available, get the games info, prices etc.
+// All of this info can be used on any page, for those who wish to use javascript to render the games details etc.
+const products = [
+    {
+        title: "Battlefield™ 1 Revolution",
+        genre: "Action",
+        image: "./img/products/battlefield-1.jpg",
+        price: 499
+    },
+    {
+        title: "Grand Theft Auto V Enhanced",
+        genre: "Action",
+        image: "./img/products/gtav.jpg",
+        price: 299
+    },
+    {
+        title: "Ready or Not",
+        genre: "Action",
+        image: "./img/products/readyornot.png",
+        price: 599
+    },
+    {
+        title: "Borderlands 4",
+        genre: "Action",
+        image: "./img/products/borderlands4.jpg",
+        price: 499,
+        onSalePrice: 299
+    },
+    {
+        title: "EA Sports FC 26",
+        genre: "Sport",
+        image: "./img/products/fc26.jpg",
+        price: 499
+    },
+    {
+        title: "Arc Raiders",
+        genre: "Action",
+        image: "./img/products/arc-raiders.jpg",
+        price: 499,
+        onSalePrice: 299
+    },
+    {
+        title: "Out of Worlds",
+        genre: "RPG",
+        image: "./img/products/outofworlds.jpg",
+        price: 499,
+        onSalePrice: 299
+    },
+    {
+        title: "Hogwarts Legacy",
+        genre: "RPG",
+        image: "./img/products/hogwartslegacy.jpg",
+        price: 349
+    },
+    {
+        title: "Red Dead Redemption 2",
+        genre: "Action",
+        image: "./img/products/reddeadredemption2.jpg",
+        price: 199
+    },
+    {
+        title: "Subnautica 2",
+        genre: "Adventure",
+        image: "./img/products/subnautica-2.png",
+        price: 499,
+        onSalePrice: 299
+    },
+    {
+        title: "The Bornless",
+        genre: "Horror",
+        image: "./img/products/bornless.png",
+        price: 499,
+        onSalePrice: 459
+    },
+    {
+        title: "ARK: Survival Ascended",
+        genre: "Adventure",
+        image: "./img/products/ark-survival-ascended.jpg",
+        price: 499
+    },
+    {
+        title: "Bloodstained",
+        genre: "RPG",
+        image: "./img/products/bloodstained.jpg",
+        price: 499,
+        onSalePrice: 245
+    },
+    {
+        title: "We Were Here Together",
+        genre: "CO-OP",
+        image: "./img/products/wewerehere.png",
+        price: 499,
+        onSalePrice: 100
+    },
+    {
+        title: "Hades II",
+        genre: "RPG",
+        image: "./img/products/hadesii.png",
+        price: 499,
+        onSalePrice: 299
+    }
+];
